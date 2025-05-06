8d10fffd4fd3754b891bab67d634a032ebf9a469
CaroLeonardo <carolileonardo01@gmail.com>
2025-05-06 12:43:39 +0200

    Update First_code.R

--- First_code.R
+++ First_code.R
@@ -1,7 +1,14 @@
-install.packages("terra")
-library(terra)  # package used for remote sensing data
+install.packages ("terra")
+library (terra)  # package used for remote sensing data
 
 # importing data
-setwd("C:/Users/carol/Desktop")
+setwd ("C:/Users/carol/Desktop")
 
 rast ("Matogrosso_l5_1992219_lrg.jpg")
+
+# Bands:
+# band 1: near infrared
+# band 2: red
+# band 3: green
+
+plotRGB (mato, r=2, g=3, b=1)
